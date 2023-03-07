72cdf94ce41366390a6ccef7620851b1eeb9393e
Vlada Shudegova <91027196+VladaShudegova@users.noreply.github.com>
2023-03-07 13:43:32 +0700

    Add foled with first Labs

---
+++ ExternalSorts/ExternalSorts.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using std::cout;
+using std::string;
+
+bool sortFile(const std::string& fileName);
+
+bool createFileWithNumbers(const std::string& fileName, const int numbersCount, const int maxNumberValue);
+
+bool createFileWithRandomNumbers(const std::string& fileName, const int numbersCount, const int maxNumberValue);
+
+bool isFileContainsSortedArray(const std::string& fileName);
+
+int createAndSortFile(const std::string& fileName, const int numbersCount, const int maxNumberValue)
+{
+    if (!createFileWithNumbers(fileName, numbersCount, maxNumberValue)) {
+        return -1;
+    }
+
+    sortFile(fileName); //Вызов вашей функции сортировки
+
+    if (!isFileContainsSortedArray(fileName)) {
+        return -2;
+    }
+
+    return 1;
+}
+
+int main()
+{
+    std::string fileName = "file.txt";
+    const int numbersCount = 1000000;
+    const int maxNumberValue = 100000;
+
+    for (int i = 0; i < 10; i++) {
+        switch (createAndSortFile(fileName, numbersCount, maxNumberValue)) {
+        case 1:
+            std::cout << "Test passed." << std::endl;
+            break;
+
+        case -1:
+            std::cout << "Test failed: can't create file." << std::endl;
+            break;
+
+        case -2:
+            std::cout << "Test failed: file isn't sorted." << std::endl;
+            break;
+        }
+
+        return 0;
+    }
